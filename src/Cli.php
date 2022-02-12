d395f3d3c17df561a02af85184988febc5d62d4b
Dmitry <ddm14159@gmail.com>
2022-02-12 14:47:19 +0300

    add cli.php

---
+++ src/Cli.php
@@ -0,0 +1,12 @@
+<?php
+    namespace Brain\Games\Cli;
+    require_once __DIR__ . '/../vendor/autoload.php';
+    use function cli\line;
+    use function cli\prompt;
+    
+    function greeting()
+    {
+        line('Welcome to the Brain Games!');
+        $name = prompt('May I have your name?');
+        line("Hello, %s!", $name);
+    }
