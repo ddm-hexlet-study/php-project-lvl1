5a1e5faf8f90bfc434b98fd1e84c41435b96de2d
Dmitry <ddm14159@gmail.com>
2022-02-12 16:30:53 +0300

    add actions

--- makefile
+++ makefile
@@ -4,3 +4,5 @@ brain-games: #запуск стартовой страницы
 	./bin/brain-games
 validate: #проверка валидности пакета
 	composer validate
+lint: #запуск phpcs
+	composer exec --verbose phpcs -- --standard=PSR12 src bin
--- src/Cli.php
+++ src/Cli.php
@@ -1,12 +1,16 @@
 <?php
-    namespace Brain\Games\Cli;
-    require_once __DIR__ . '/../vendor/autoload.php';
-    use function cli\line;
-    use function cli\prompt;
-    
-    function greeting()
-    {
+
+namespace Brain\Games\Cli;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use function cli\line;
+use function cli\prompt;
+
+
+function greeting()
+{
         line('Welcome to the Brain Games!');
         $name = prompt('May I have your name?');
         line("Hello, %s!", $name);
-    }
+}
