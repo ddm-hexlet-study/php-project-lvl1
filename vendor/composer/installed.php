1752b4a7ad7c28ae072813f6977f08aca4b7ec25
Dmitry <ddm14159@gmail.com>
2022-02-12 16:40:30 +0300

    delete /vendor from .gitignore

---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'ff3ccd3588642f44944f757b465d5fd1bff21af8',
+        'name' => 'hexlet/code',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hexlet/code' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'ff3ccd3588642f44944f757b465d5fd1bff21af8',
+            'dev_requirement' => false,
+        ),
+        'squizlabs/php_codesniffer' => array(
+            'pretty_version' => '3.6.2',
+            'version' => '3.6.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../squizlabs/php_codesniffer',
+            'aliases' => array(),
+            'reference' => '5e4e71592f69da17871dba6e80dd51bce74a351a',
+            'dev_requirement' => true,
+        ),
+        'wp-cli/php-cli-tools' => array(
+            'pretty_version' => 'v0.11.13',
+            'version' => '0.11.13.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wp-cli/php-cli-tools',
+            'aliases' => array(),
+            'reference' => 'a2866855ac1abc53005c102e901553ad5772dc04',
+            'dev_requirement' => false,
+        ),
+    ),
+);
